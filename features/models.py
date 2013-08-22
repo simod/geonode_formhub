1f6e11f46f2a584eb6df88ba6a39848bb96e7c15
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-08-22 10:46:11 +0200

    move image to a char to avoid image duplications and fix some issues

--- features/models.py
+++ features/models.py
@@ -4,12 +4,13 @@ from geonode.layers.models import Layer
 
 class Feature(models.Model):
 
+    feature_id = models.IntegerField(primary_key=True)
     lat = models.FloatField()
     lon = models.FloatField()
 
     layer = models.ForeignKey(Layer)
 
-    image = models.ImageField(upload_to='features', blank=True, null=True)
+    image = models.CharField(max_length=128, blank=True, null=True)
 
     def __unicode__(self):
         return 'Feature'
--- formhub/views.py
+++ formhub/views.py
@@ -1,4 +1,5 @@
 import json
+import psycopg2
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -17,27 +18,32 @@ wfs_url = settings.GEOSERVER_BASE_URL + "wfs/WfsDispatcher?"
 def index(req):
     pass
 
-def compile_context(req_body, attributes):
+def compile_context(valid_id, req_body, attributes):
     context = {
             'fields': []
         }
+    image = None
     for a in attributes:
         # For each attribute in the layer look for the correspondent in the form
         # TODO: do it safely
-        if a.attribute != 'the_geom' and a.attribute != 'id':
+        if a.attribute != 'the_geom' and a.attribute != 'id' and a.attribute != 'image':
             context['fields'].append(
                 {
                 'name': a.attribute,
                 'value': req_body[a.attribute]
                 }
             )
+        if len(req_body['_attachments']) > 0:
+            image = req_body['_attachments'][0]
+
     lat = req_body['_geolocation'][0] if req_body['_geolocation'][0] is not None else 0
     lon = req_body['_geolocation'][1] if req_body['_geolocation'][1] is not None else 0
     context['lat'] = lat
     context['lon'] = lon
     context['layername'] = req_body['_xform_id_string']
-    context['id'] = req_body['_id']
-    return context
+    context['id'] = valid_id
+
+    return context, image
 
 @csrf_exempt
 @require_POST
@@ -46,24 +52,59 @@ def form_save(req):
     check_geonode_is_up()
 
     body = json.loads(req.body)
-    
-    geoserver = Gs_client(wfs_url)
-
+    print body
     layername = body['_xform_id_string']
     try:
         layer = Layer.objects.get(title=layername)
     except Layer.DoesNotExist:
         raise Layer.DoesNotExist
-
+    # Don't trust the id from ODK, look into the db sequence to get the right one
+    valid_id = get_valid_id(layername)
     attributes = layer.attribute_set.all()
-    
-    context = compile_context(body, attributes)
-    
+    context, image = compile_context(valid_id, body, attributes)
     template = render_to_string('formhub/transaction_insert.xml',context)
+
+    # Instanciate the client and post the insert request
+    geoserver = Gs_client(wfs_url)
     try:
         response = geoserver.client.request(wfs_url, method='POST', body=template)
+
+        #Update the layer bounds and the thumbnail
         geoserver.updatebounds(layername)
         layer.update_thumbnail()
+        context_to_feature(layer, context, image)
         return HttpResponse('Inserted')
     except:
         raise 
+
+def context_to_feature(layer, context, image):
+    """ Write the feature with the image to the database
+    """
+    try:
+        Feature.objects.create(
+            feature_id = context['id'],
+            lat = context['lat'],
+            lon = context['lon'],
+            layer = layer,
+            image = image
+            )
+        return True
+    except:
+        raise
+
+def get_valid_id(layername):
+    """ Get a valid id from the layer sequence from the database
+    """
+    connection = psycopg2.connect(
+        host=settings.DB_DATASTORE_HOST,
+        database=settings.DB_DATASTORE_DATABASE,
+        user=settings.DB_DATASTORE_USER,
+        password=settings.DB_DATASTORE_PASSWORD,
+        port=settings.DB_DATASTORE_PORT
+        )
+    cursor=connection.cursor()
+    cursor.execute('SELECT last_value FROM %s_fid_seq;' % layername)
+    valid_id = cursor.fetchone()[0] + 1
+    cursor.close()
+    connection.close()
+    return valid_id

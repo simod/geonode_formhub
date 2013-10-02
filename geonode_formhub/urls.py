1ad963e28af23da7e8f9fabcb00cb381bedf15ad
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-10-02 15:51:32 +0200

    fix url

--- geonode_formhub/urls.py
+++ geonode_formhub/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, patterns
  
 urlpatterns = patterns('',
     (r'^save/?$', include('geonode_formhub.formhub.urls')),
-    (r'^crowd_layers/?', include('geonode_formhub.features.urls')),
+    (r'^crowd_layers/', include('geonode_formhub.features.urls')),
     )

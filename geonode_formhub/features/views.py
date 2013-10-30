4c98558c96261379b22187837b744986ef028fca
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-10-30 15:07:42 +0100

    check postgis is configured before activating the crowd extension

--- geonode_formhub/features/views.py
+++ geonode_formhub/features/views.py
@@ -8,17 +8,24 @@ from geonode.layers.views import _resolve_layer, _PERMISSION_MSG_VIEW
 from geonode.search.search import _filter_security
 
 from .models import Feature
+from geonode_formhub.formhub.utils import check_feature_store
 
 def crowd_layers(request, template='features/crowd_layers.html'):
-    layers = Layer.objects.filter(pk__in=Feature.objects.distinct('layer').values('layer'))
-    layers = _filter_security(layers, request.user, Layer, 'view_layer')
-    return render_to_response(
-        template,
-        RequestContext(request, {
-            "layers": layers,
-            }
+    if check_feature_store():
+        layers = Layer.objects.filter(pk__in=Feature.objects.distinct('layer').values('layer'))
+        layers = _filter_security(layers, request.user, Layer, 'view_layer')
+        return render_to_response(
+            template,
+            RequestContext(request, {
+                "layers": layers,
+                }
+            )
         )
-    )
+    else:
+        return render_to_response(
+            'features/disabled.html',
+            RequestContext(request)
+        )
 
 def crowd_layer_detail(request, layername, template='features/crowd_layer_detail.html'):
     layer = _resolve_layer(request, layername, 'layers.view_layer', _PERMISSION_MSG_VIEW)
